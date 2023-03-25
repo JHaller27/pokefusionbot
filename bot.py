c928c9045d0a85ceb722d3d04900f3db089922fa
James <jameshaller27@gmail.com>
2023-03-24 22:57:25 -0700

    Extract fuse()

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import discord
-import main
+import fusion
 import os
 
 from typing import Optional
@@ -8,28 +8,31 @@ from typing import Optional
 class MyClient(discord.Client):
 	async def on_ready(self):
 		print(f'Logged on as {self.user}')
-		self.id_dict = main.load_pokedict()
+		self.id_dict = fusion.load_pokedict()
 		self.dev_mode = os.getenv('DEVMODE', 'false').lower() == 'true'
 		print('Ready')
 
 	async def on_message(self, message: discord.Message):
 		if message.content.startswith('!'):
 			args = message.content.split()
+
 			if args[0] == '!fuse' and len(args) == 3:
 				_, p1, p2 = args
+				self.fuse(message, p1, p2)
 
-				id1 = self.id_dict.get(p1.lower())
-				if id1 is None:
-					await self.send_reply(message, f"**I'm sorry, I don't recognize {p1}.** :cry:")
-					return
+	async def fuse(self, message: discord.Message, p1: str, p2: str) -> None:
+		id1 = self.id_dict.get(p1.lower())
+		if id1 is None:
+			await self.send_reply(message, f"**I'm sorry, I don't recognize {p1}.** :cry:")
+			return
 
-				id2 = self.id_dict.get(p2.lower())
-				if id2 is None:
-					await self.send_reply(message, f"**I'm sorry, I don't recognize {p2}.** :cry:")
-					return
+		id2 = self.id_dict.get(p2.lower())
+		if id2 is None:
+			await self.send_reply(message, f"**I'm sorry, I don't recognize {p2}.** :cry:")
+			return
 
-				url = main.get_fusion_url(id1, id2)
-				await self.send_embed(message, url, f'{p1}/{p2}')
+		url = fusion.get_fusion_url(id1, id2)
+		await self.send_embed(message, url, f'{p1}/{p2}')
 
 	def get_reply(self, message: discord.Message, text: str = '') -> str:
 		reply = f'{message.author.mention}\n{text}'
