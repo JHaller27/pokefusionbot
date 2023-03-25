e36d27cefef6c507dfb667c96c224a2c3405b8a0
James <jameshaller27@gmail.com>
2023-03-24 22:52:31 -0700

    Add proper 'not found' message

--- bot.py
+++ bot.py
@@ -15,25 +15,38 @@ class MyClient(discord.Client):
 	async def on_message(self, message: discord.Message):
 		if message.content.startswith('!'):
 			args = message.content.split()
-			if args[0] == '!fuse':
+			if args[0] == '!fuse' and len(args) == 3:
 				_, p1, p2 = args
 
 				id1 = self.id_dict.get(p1.lower())
-				id2 = self.id_dict.get(p2.lower())
+				if id1 is None:
+					await self.send_reply(message, f"**I'm sorry, I don't recognize {p1}.** :cry:")
+					return
 
-				print(f'{id1=} {id2=}')
+				id2 = self.id_dict.get(p2.lower())
+				if id2 is None:
+					await self.send_reply(message, f"**I'm sorry, I don't recognize {p2}.** :cry:")
+					return
 
 				url = main.get_fusion_url(id1, id2)
-				await self.embed_img(message, url, f'{p1}/{p2}')
+				await self.send_embed(message, url, f'{p1}/{p2}')
 
-	async def embed_img(self, message: discord.Message, url: str, description: Optional[str] = None):
+	def get_reply(self, message: discord.Message, text: str = '') -> str:
+		reply = f'{message.author.mention}\n{text}'
+
+		if self.dev_mode:
+			reply = f'DEVMODE\n{reply}'
+
+		return reply
+
+	async def send_reply(self, message: discord.Message, text: str) -> None:
+		await message.channel.send(self.get_reply(message, text))
+
+	async def send_embed(self, message: discord.Message, url: str, description: Optional[str] = None):
 		embed = discord.Embed(description=description)
 		embed.set_image(url=url)
 
-		msg = message.author.mention
-
-		if self.dev_mode:
-			msg = f'DEVMODE\n{msg}'
+		msg = self.get_reply(message)
 
 		await message.channel.send(msg, embed=embed)
 
